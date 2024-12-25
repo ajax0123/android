a98d2712658712f4190d3f207f4f47f01b9ba96c
ajax0123 <ap05090907@gmail.com>
2024-12-25 19:59:29 +0530

    Create Arithmetic.dart

---
+++ Arithmetic.dart
@@ -0,0 +1,17 @@
+void main(){
+  int a = 10;
+  int b = 5;
+ 
+  int sum = a+b;
+  print('The Sum Of a + b is: ${sum}');
+
+  int diff = a-b;
+  print('The Difference of a and b is: ${diff}');
+  
+  int product = a*b;
+  print('The product of a and b is: ${product} ');
+
+  double Quotient = a/b;
+  print('The Quotient Of a and b is: ${Quotient}');
+  
+}
