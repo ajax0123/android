1ebe9a40d0a26efd7e85d91a2ec7d0416b2097c2
ajax0123 <ap05090907@gmail.com>
2024-12-25 20:07:55 +0530

    Create WorkingWithMaps.dart

---
+++ WorkingWithMaps.dart
@@ -0,0 +1,10 @@
+void  main(){
+  Map<String , dynamic> mapData = {
+    'Name':'Alice',
+    'age':25,
+    'city':'New York'
+  };
+  mapData['Country']='USA';
+  mapData['age'] = 26;
+  print(mapData);
+}
